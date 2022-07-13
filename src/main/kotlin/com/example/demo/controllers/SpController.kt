cb6b349a8bfe0782a6e60c618f0217ea48253cfe
kenji sato <63083111+pa-k-sato@users.noreply.github.com>
2022-07-13 15:42:48 +0900

    add exception handler

--- src/main/kotlin/com/example/demo/controllers/SpController.kt
+++ src/main/kotlin/com/example/demo/controllers/SpController.kt
@@ -1,8 +1,12 @@
 package com.example.demo.controllers
 
 import com.example.demo.config.ExperimentalApiReleaseToggle
+import com.example.demo.config.NotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,6 +19,10 @@ class ExperimentalController(
         toggle.assertReleased()
         return "experimental tasks"
     }
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFound() {}
 }
 
 @RestController
